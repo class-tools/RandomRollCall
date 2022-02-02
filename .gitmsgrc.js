d7580a2e7d0135268619bb83fe99bb02340ee336
ren-yc <ldfx_ryc@163.com>
2022-02-02 17:16:28 +0800

    VER 1.0.0
    *@ commit-std
     : Create Action Running YAML file.

---
+++ .gitmsgrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	debug: false,
+	itemTy: o => o.concat("MHDU".split("")),
+	checker: {
+		itemTy: N => {
+		if (N.l === 3 && N.c === "-")
+			throw `is root item, so shouldn't be "-" type`
+		}
+	}
+}
